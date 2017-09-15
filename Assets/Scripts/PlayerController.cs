098017551f806d0cfc8d8e1a666974e937a57b33
Adam Leung <centribo@gmail.com>
2017-09-15 19:11:43 -0400

    Updated Unity version and updated some public values

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -43,7 +43,7 @@ public class PlayerController : MonoBehaviour {
 	void MoveHorizontal(float input){ //Takes a input from -1.0 to 1.0
 		Vector2 moveVel = rb.velocity; //Get our current rigidbody's velocity
 		moveVel.x = input * speed * Time.deltaTime; //Set the new x velocity to be the given input times our speed
-							    //Note the multiply by Time.deltaTime to compensate for game clock
+		//Note the multiply by Time.deltaTime to compensate for game clock
 		rb.velocity = moveVel; //Update our rigidbody's velocity
 	}
 
